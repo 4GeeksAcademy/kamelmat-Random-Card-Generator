2973b54c23b9af972164124f3edbcbba39ef1651
Matias Kamelman <125071787+kamelmat@users.noreply.github.com>
2024-04-08 19:25:37 +0000

    feat timer final

--- src/app.js
+++ src/app.js
@@ -20,51 +20,29 @@ function changeSuit() {
   let selectedSuit = suits[randomIndex];
   topSuitElement.innerHTML = bottomSuitElement.innerHTML = suits[randomIndex];
   if (selectedSuit === "♥" || selectedSuit === "♦") {
-  topSuitElement.style.color = "red";
-  bottomSuitElement.style.color = "red";
-  cardNumberElement.style.color = "red";
+    topSuitElement.style.color = "red";
+    bottomSuitElement.style.color = "red";
+    cardNumberElement.style.color = "red";
   } else {
-  topSuitElement.style.color = "black";
-  bottomSuitElement.style.color = "black";
-  cardNumberElement.style.color = "black";
-   }
+    topSuitElement.style.color = "black";
+    bottomSuitElement.style.color = "black";
+    cardNumberElement.style.color = "black";
+  }
   bottomSuitElement.style.transform = "rotate(180deg)";
-    
-  }
-  function changeNumber () {
-    let figures = ["A", "J", "Q", "K"];
-    const randomNumber = () => Math.floor(Math.random() * 9) + 2;
-    const randomFigures = () => figures[Math.floor(Math.random() * figures.length)];
 
-    let changedCardNumber = Math.random() < 0.5 ? randomNumber() : randomFigures();
-    cardNumberElement.innerHTML = changedCardNumber;
-  }
+}
+function changeNumber() {
+  let figures = ["A", "J", "Q", "K"];
+  const randomNumber = () => Math.floor(Math.random() * 9) + 2;
+  const randomFigures = () => figures[Math.floor(Math.random() * figures.length)];
 
-  let timeLeft = 10;
+  let changedCardNumber = Math.random() < 0.5 ? randomNumber() : randomFigures();
+  cardNumberElement.innerHTML = changedCardNumber;
+}
 
-  function countdown() {
-    timeLeft--;
-    document.getElementById("seconds").innerHTML = String( timeLeft );
-    if (timeLeft > 0) {
-      setTimeout(countdown, 1000);
-    }
-  };
-
-  setTimeout(countdown, 1000);
-  function startTimer() {
-  let seconds = 10;
-  const timerElement = document.querySelector("#seconds");
-
-  const timerInterval = setInterval(() => {
-    if (seconds === 0) {
-      clearInterval(timerInterval);
-      changeSuit();
-      changeNumber();
-      startTimer();
-    } else {
-      timerElement.textContent = seconds;
-      seconds--;
-    }
-  }, 1000);
-  }
-
+function startTimer() {
+  setInterval(() => {
+    changeSuit();
+    changeNumber();
+  }, 10000);
+}
